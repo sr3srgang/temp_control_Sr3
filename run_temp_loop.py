c9ad9b83114b0a9ea8413017fd518306a5e6c8c1
sr3srgang <sr3srgang@gmail.com>
2021-12-13 15:11:32 -0700

    set up on momo

--- run_temp_loop.py
+++ run_temp_loop.py
@@ -7,7 +7,7 @@ from Keithley import Keithley
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     PID_params = {"setpoint": 30, "dt": 5}
-    k = Keithley('10.1.140.167', 5025) 
+    k = Keithley('192.168.1.25', 5025) 
     loop=TempLoop("Laser-side temp loop", k, None, PID_params)
     loop.show()
     sys.exit(app.exec_())
