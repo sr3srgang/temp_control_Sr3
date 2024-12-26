574f29370224de71885c3701ef5f7472ea0bd537
sr3srgang <sr3srgang@gmail.com>
2024-12-26 16:35:49 -0700

    Set temp increased: 19.6 -> 20.5

--- run_temp_loop.py
+++ run_temp_loop.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     #PID_params = {"k_prop": -.35, "t_int": 60, "t_diff": 0, "setpoint":19.2,  "dt": 15, "output_default":2.5, "rails": [1, 5]}
     #MM 20221227 turned down gain from -.25 to -.2 bc servo was ringing. 
-    PID_params = {"k_prop": -.15, "t_int": 120, "t_diff": 0, "setpoint":19.6,  "dt": 15, "output_default":1.53, "rails": [1, 5]}
+    #PID_params = {"k_prop": -.15, "t_int": 120, "t_diff": 0, "setpoint":19.6,  "dt": 15, "output_default":1.53, "rails": [1, 5]}
+    # 20241226 JH increased set temp to lower the PCW flow to the HEPA; it was dominating the PCW for Sr3
+    PID_params = {"k_prop": -.15, "t_int": 120, "t_diff": 0, "setpoint":20.5,  "dt": 15, "output_default":1.53, "rails": [1, 5]}
     k = Keithley('192.168.1.25', 5025)
     r = Rigol('192.168.1.27', 5555, 2)  
     loop=TempLoop("Laser-side temp loop", k, r, PID_params)
