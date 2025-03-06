f27b63348be8b3e9e5393afc555d96a27d30e3e3
sr3srgang <sr3srgang@gmail.com>
2025-03-06 14:50:02 -0700

    Added type declarations and error handling.

--- TempLoop.py
+++ TempLoop.py
@@ -5,18 +5,32 @@ from PlotWindow import PlotWindow
 import os
 from PID import PID
 
+# for error handling
+import sys
+import traceback
+
+# for typing
+from typing import Union
+from Keithley import Keithley, Keithley_mux
+from Rigol import Rigol
+
 # libraries for data uploading
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 class TempLoop(QWidget):
-    def __init__(self, name, input_device, output_device, PID_params):
+    def __init__(self, name, input_device:Union[Keithley, Keithley_mux], output_device:Rigol, PID_params):
         super(TempLoop, self).__init__()
         
         self.input_device = input_device
         self.output_device = output_device
         self.PID_params = PID_params
         self.PID = PID(self.PID_params)
+        self.il = 0 # loop iteration #
+        
+        # Joon added: set rigol output to 12V
+        # so that `turn_on_rigol_12V.py` doesn't have to be run
+        # self.output_device.set_val(12)
         
         
         self.setWindowTitle(name)
@@ -81,30 +95,56 @@ class TempLoop(QWidget):
         self.PID.clear_integrator() 
                
     def update_loop(self):
-        #Read temp, update window, log. 
-        time = QDateTime.currentDateTime()
-        timeDisplay=time.toString('yyyy-MM-dd hh:mm:ss dddd')
-        self.time_label.setText(timeDisplay)
-        
-        #Read in value from Keithley, 
-        current_temp, res = self.input_device.read_temp()
-        self.temp_label.setText("Current temp: {:.3f}".format(current_temp))
-        
-        #Log temp and output
-        output = self.PID.update(current_temp)
-        f_path = self.get_fname_write(time)
-        
-        # send data to yesnuffleupagus
-        # Send to the db
-        with InfluxDBClient(url="http://yesnuffleupagus.colorado.edu:8086", token="yelabtoken", org="yelab", debug=False) as client:
-            write_api = client.write_api(write_options=SYNCHRONOUS)
-            write_api.write("data_logging", "yelab", "Sr3_temp,Channel=1 Value={}".format(current_temp))
-            client.close()
-        
-        
-        with open(f_path, 'a') as f:
-            f.write("{}, {}, {}, {}\n".format(timeDisplay, current_temp, res, output))
-        #To actuate:
-        self.output_device.set_val(output)
-        self.voltage_label.setText("Rigol V: {:.3f}".format(output))
+        try:
+            #Read temp, update window, log. 
+            time = QDateTime.currentDateTime()
+            timeDisplay=time.toString('yyyy-MM-dd hh:mm:ss dddd')
+            self.time_label.setText(timeDisplay)
             
+            #Read in value from Keithley,
+            try:
+                current_temp, res = self.input_device.read_temp()
+            except Exception as ex:
+                print("Get Keithley temp reading failed.", file=sys.stderr)
+                print(traceback.format_exception(), file=sys.stderr)
+                return
+            self.temp_label.setText("Current temp: {:.3f}".format(current_temp))
+            
+            #Log temp and output
+            print(f"[{time.toString('MM/dd hh:mm:ss')}] {self.il}: [P,I,D]=", end="")
+            output = self.PID.update(current_temp)
+            f_path = self.get_fname_write(time)
+            
+            #To actuate:
+            try:
+                self.output_device.set_val(output)
+            except Exception as ex:
+                print("Set RIGOL output voltage failed.", file=sys.stderr)
+                print(traceback.format_exception(), file=sys.stderr)
+                return
+                
+            # write log to file
+            try:
+                with open(f_path, 'a') as f:
+                    f.write("{}, {}, {}, {}\n".format(timeDisplay, current_temp, res, output))
+            except Exception as ex:
+                print("Writing log to file faield.", file=sys.stderr)
+                print(traceback.format_exception(), file=sys.stderr) 
+            
+            # Send result to influxdb
+            try:
+                with InfluxDBClient(url="http://yesnuffleupagus.colorado.edu:8086", token="yelabtoken", org="yelab", debug=False) as client:
+                    write_api = client.write_api(write_options=SYNCHRONOUS)
+                    write_api.write("data_logging", "yelab", "Sr3_temp,Channel=1 Value={}".format(current_temp))
+            except Exception as ex:
+                print("InfluxDB uploading failed.", file=sys.stderr)
+                print(traceback.format_exception(), file=sys.stderr)
+            
+
+            self.voltage_label.setText("Rigol V: {:.3f}".format(output))
+        except Exception as ex:
+            print("Unhandled exception raised. Dropping this iteration.", file=sys.stderr)
+            print(traceback.format_exception(), file=sys.stderr)
+            return
+        finally:
+            self.il = self.il + 1
